2129e5702fd93334f89718c80ad4b2b5be1b3c84
Mojeed Kusimo <mkusimo90@gmail.com>
2025-03-08 22:23:44 +0100

    Completed Header and Content Components

--- part-1/1-1/src/App.jsx
+++ part-1/1-1/src/App.jsx
@@ -1,4 +1,36 @@
 const App = () => {
+
+  const Header = (props) => {
+
+    return (
+      <h1>{props.course}</h1>
+    )
+
+  }
+
+  const Content = (props) => {
+
+    return (
+      <p>
+        {props.part} {props.exercises}
+      </p>
+    )
+
+  }
+
+  const ContentBody = (props) => {
+
+    return (
+      <>
+      <Content part={part1} exercises={exercises1}/>
+      <Content part={part2} exercises={exercises2}/>
+      <Content part={part3} exercises={exercises3}/>
+      </>
+    )
+
+  }
+
+
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
@@ -9,16 +41,8 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Header course={course}/>
+      <ContentBody />
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
