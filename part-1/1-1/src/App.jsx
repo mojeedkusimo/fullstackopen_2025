62eba3a4c4b9c4ae0e9435e8d1accfeabc062341
Mojeed Kusimo <mkusimo90@gmail.com>
2025-03-09 21:38:26 +0100

    Complete task 1.2

--- part-1/1-1/src/App.jsx
+++ part-1/1-1/src/App.jsx
@@ -7,20 +7,24 @@ const App = () => {
     )
 
   }
+  
+  const Part = (props) => {
 
-  const Content = (props) => {
+    return (
+        <p>
+          {props.part} {props.exercises}
+        </p>
+    )
+
+  }
+
+  const Content = () => {
 
     return (
       <>
-        <p>
-          {props.part1} {props.exercises1}
-        </p>
-        <p>
-          {props.part2} {props.exercises2}
-        </p>
-        <p>
-          {props.part3} {props.exercises3}
-        </p>
+        <Part part={part1} exercises={exercises1}/>
+        <Part part={part2} exercises={exercises2}/>
+        <Part part={part3} exercises={exercises3}/>
       </>
     )
 
@@ -35,6 +39,7 @@ const App = () => {
   }
 
 
+
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
@@ -46,11 +51,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content
-        part1={part1} exercises1={exercises1}
-        part2={part2} exercises2={exercises2}
-        part3={part3} exercises3={exercises3}
-      />
+      <Content />
       <Total
         exercises1={exercises1}
         exercises2={exercises2}
