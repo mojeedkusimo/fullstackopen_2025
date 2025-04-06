4b3a3f1c2286648a2fc436a743756df1c1cbd0f4
Mojeed Kusimo <mkusimo90@gmail.com>
2025-04-06 03:42:35 +0100

    feat: Complete task 3.5

---
+++ part-3/3-5/index.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+app.use(express.json());
+
+const persons = [
+  {
+    "id": "1",
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": "2",
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": "3",
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": "4",
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
+];
+
+
+
+app.get('/', (req, res) => {
+  return res.send('Hello World! express');
+});
+
+app.get('/api/persons', (req, res) => {
+  return res.json(persons);
+});
+
+app.get('/info', (req, res) => {
+  return res.send(`Phonebook has info for ${persons.length} people <br><br> ${new Date()}`);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = req.params.id;
+  const person = persons.find(p => p.id === id);
+  if (person) {
+    return res.json(person);
+  } else {
+    return res.status(404).send('Person not found');
+  }
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = req.params.id;
+  const remainingPersons = persons.filter(p => p.id !== id);
+  if (remainingPersons.length < persons.length) {
+    return res.status(204).end();
+  } else {
+    return res.status(404).send('Person not found');
+  }
+});
+
+app.post('/api/persons', (req, res) => {
+  const newPerson = req.body;
+  if (!newPerson.name || !newPerson.number) {
+    return res.status(400).json({ error: 'Name or number is missing' });
+  }
+  newPerson.id = (Math.random() * 1000).toString(); // Simple ID generation
+  const newPersons = persons.concat(newPerson);
+  console.log(newPersons);
+  return res.status(201).json(newPerson);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
